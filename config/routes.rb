7a88e4933fff00a982bcf73a3d0becb7deec29dc
Freddie Fryer <freddiefryercode@gmail.com>
2021-01-30 17:13:43 +0000

    rake all green for models/controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :new, :create, :index, :show  ] do
+  resources :cocktails, only: [ :new, :create, :index, :show ] do
     resources :doses, only: [ :new, :create ]
   end
-
+  resources :doses, only: [ :destroy ]
 end
