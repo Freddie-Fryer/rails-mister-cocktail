1bd8a10d3f73e3e862b13c1af2b93a0b1e002313
Freddie Fryer <freddiefryercode@gmail.com>
2021-02-05 19:34:35 +0000

    lots of styling done

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,6 +8,27 @@ class IngredientsController < ApplicationController
     end
   end
 
+  def show
+    @ingredient = Ingredient.find(params[:id])
+    @cocktails = Cocktails.all
+    @sorted_cocktails = @cocktails.sort_by { |obj| obj.name.downcase }
+
+    query = <<-SQL
+      SELECT cocktails.name
+      FROM cocktails 
+      JOIN albums ON tracks.album_id = albums.id
+      JOIN artists ON albums.artist_id = artists.id
+      JOIN genres ON tracks.genre_id = genres.id
+      WHERE genres.name = "#{genre_name}"
+      GROUP BY artists.name
+      ORDER BY c  DESC
+      LIMIT 5
+    SQL
+    @cocktails_by_ingredient = db.execute(query)
+  end
+
+  private
+
   def ingredient_params
     params.require(:ingredient).permit(:name)
   end
